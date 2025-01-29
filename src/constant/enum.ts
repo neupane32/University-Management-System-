f4fc75a6a16130ca493e94326eab6fb99d89f503
Aashish Neupane <120081944+neupane32@users.noreply.github.com>
2025-01-29 16:09:06 +0545

    added base and admin entity

---
+++ src/constant/enum.ts
@@ -0,0 +1,28 @@
+export enum Environment {
+    DEVELOPMENT = 'DEVELOPMENT',
+    PRODUCTION = 'PRODUCTION',
+  }
+  
+  export enum MediaType {
+    POST = 'POST',
+  }
+  
+  export enum Role {
+    ADMIN = 'ADMIN',
+    UNIVERSITY = 'UNIVERSITY',
+    TEACHER = 'TEACHER',
+    STUDNET = 'STUDENT'
+  
+  }
+  export enum Status {
+    ACCEPTED = 'ACCEPTED',
+    PENDING = 'PENDING',
+    BLOCKED = 'BLOCKED',
+  }
+  
+  export enum Gender {
+    MALE = 'MALE',
+    FEMALE = 'FEMALE',
+    NULL = 'NULL',
+  }
+  
