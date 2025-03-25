1f402bbacb3c5cd33a67d4eec651d00e9dd0b98c
Aashish Neupane <120081944+neupane32@users.noreply.github.com>
2025-03-26 00:32:31 +0545

    completed  crud of the Subscription.

--- src/entities/Subscription/subscription.entity.ts
+++ src/entities/Subscription/subscription.entity.ts
@@ -4,25 +4,33 @@ import { Entity, Column } from "typeorm";
 @Entity("Subscription")
 export class Subscription extends Base {
   @Column({
-    unique: true,
+
     nullable: false,
   })
   title: string;
 
   @Column({
-    unique: true,
+   
     type: 
     'float'
   })
   duration: number;
 
   @Column({
-    unique: true,
+   
+    type: 
+    'float'
+  })
+  price: number;
+
+
+  @Column({
+    
     type: 
     'float'
   })
   bonus: number;
 
-  @Column({ select: false, nullable: false, default: false })
-  mostPoopular: boolean;
+  @Column({  nullable: false, default: false })
+  mostPopular: boolean;
 }
